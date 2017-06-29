fc1bc3bc99a4fb0a0ca79058650317004a355524
pavlichenko86 <vovan4ik86mail@gmail.com>
2017-06-29 23:30:48 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+
+ alert(document.querySelector('#one').textContent);
+
+document.querySelector('#two').textContent += (' Hello World');
+
+document.querySelector('#ttt').innerHTML = ('<b> Hello World</b>');
